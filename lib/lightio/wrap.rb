a85f15432e12097e9f8d76ec4fe1108914408e84
jjy <jjyruby@gmail.com>
2017-12-24 02:01:51 +0800

    split Wrapper and IOWrapper

--- lib/lightio/wrap.rb
+++ lib/lightio/wrap.rb
@@ -1,62 +1,18 @@
 module LightIO::Wrap
   WRAPPERS = {}
+  RAW_IO = ::IO
+
+  # wrapper for normal ruby objects
   module Wrapper
-    # wrap raw ruby io objects
+    # wrap raw ruby objects
     #
-    # @param [IO, Socket]  io raw ruby io object
+    # @param [Object] io raw ruby object, param name is 'io' just for consistent
     def initialize(io=nil)
       @io ||= io
-      @io_watcher ||= LightIO::Watchers::IO.new(@io)
     end
 
     def method_missing(method, *args)
       @io.public_send(method, *args)
-    end
-
-    def close(*args)
-      # close watcher before io closed
-      @io_watcher.close
-      @io.close(*args)
-    end
-
-    def shutdown(*args)
-      # close watcher before io shutdown
-      @io_watcher.close
-      @io.shutdown(*args)
-    end
-
-    protected
-    def _wrap(io=nil)
-      @io ||= io
-      @io_watcher ||= LightIO::Watchers::IO.new(@io)
-    end
-
-    # wait io nonblock method
-    #
-    # @param [Symbol]  method method name, example: wait_nonblock
-    # @param [args] args arguments pass to method
-    def wait_nonblock(method, *args, exception_symbol: true)
-      loop do
-        begin
-          result = if RUBY_VERSION > "2.3" && exception_symbol
-                     @io.__send__(method, *args, exception: false)
-                   else
-                     @io.__send__(method, *args)
-                   end
-          case result
-            when :wait_readable
-              @io_watcher.wait_readable
-            when :wait_writable
-              @io_watcher.wait_writable
-            else
-              return result
-          end
-        rescue IO::WaitReadable
-          @io_watcher.wait_readable
-        rescue IO::WaitWritable
-          @io_watcher.wait_writable
-        end
-      end
     end
 
     # both works in class scope and singleton class scope
@@ -91,18 +47,6 @@ module LightIO::Wrap
       include SingletonClassCommonMethods
 
       protected
-      # wrap blocking method with "#{method}_nonblock"
-      #
-      # @param [Symbol]  method method name, example: wait
-      def wrap_blocking_method(method, exception_symbol: true)
-        define_method method do |*args|
-          wait_nonblock(:"#{method}_nonblock", *args, exception_symbol: exception_symbol)
-        end
-      end
-
-      def wrap_blocking_methods(*methods, exception_symbol: true)
-        methods.each {|m| wrap_blocking_method(m, exception_symbol: exception_symbol)}
-      end
 
       attr_reader :raw_class
 
@@ -118,14 +62,74 @@ module LightIO::Wrap
     end
 
     class << self
-      def new_and_wrap(*args)
-        io = raw_class.new(*args)
-        _wrap(io)
-      end
-
       def included(base)
         base.send :extend, ClassMethods
         base.singleton_class.send :extend, SingletonClassCommonMethods
+      end
+    end
+  end
+
+  # wrapper for ruby io objects
+  module IOWrapper
+    include Wrapper
+    # wrap raw ruby io objects
+    #
+    # @param [IO, Socket]  io raw ruby io object
+    def initialize(io=nil)
+      super
+      @io_watcher ||= LightIO::Watchers::IO.new(@io)
+    end
+
+    protected
+    # wait io nonblock method
+    #
+    # @param [Symbol]  method method name, example: wait_nonblock
+    # @param [args] args arguments pass to method
+    def wait_nonblock(method, *args, exception_symbol: true)
+      loop do
+        begin
+          result = if RUBY_VERSION > "2.3" && exception_symbol
+                     @io.__send__(method, *args, exception: false)
+                   else
+                     @io.__send__(method, *args)
+                   end
+          case result
+            when :wait_readable
+              @io_watcher.wait_readable
+            when :wait_writable
+              @io_watcher.wait_writable
+            else
+              return result
+          end
+        rescue IO::WaitReadable
+          @io_watcher.wait_readable
+        rescue IO::WaitWritable
+          @io_watcher.wait_writable
+        end
+      end
+    end
+
+    module ClassMethods
+      include Wrapper::ClassMethods
+      protected
+      # wrap blocking method with "#{method}_nonblock"
+      #
+      # @param [Symbol]  method method name, example: wait
+      def wrap_blocking_method(method, exception_symbol: true)
+        define_method method do |*args|
+          wait_nonblock(:"#{method}_nonblock", *args, exception_symbol: exception_symbol)
+        end
+      end
+
+      def wrap_blocking_methods(*methods, exception_symbol: true)
+        methods.each {|m| wrap_blocking_method(m, exception_symbol: exception_symbol)}
+      end
+    end
+
+    class << self
+      def included(base)
+        Wrapper.included(base)
+        base.send :extend, ClassMethods
       end
     end
   end
