17338a712d507ac8b6b6c21fc24c16f94015a451
jjy <jjyruby@gmail.com>
2018-02-02 19:56:50 +0800

    update file_spec.rb

--- spec/lightio/library/file_spec.rb
+++ spec/lightio/library/file_spec.rb
@@ -11,4 +11,18 @@ RSpec.describe LightIO::Library::IO do
       f.close
     end
   end
+
+  describe "#pipe" do
+    it "works with beam" do
+      r1, w1 = LightIO::Library::File.pipe
+      r2, w2 = LightIO::Library::File.pipe
+      b1 = LightIO::Beam.new {r1.gets}
+      b2 = LightIO::Beam.new {r2.gets}
+      b1.join(0.01); b2.join(0.01)
+      w1.puts "foo "
+      w2.puts "bar"
+      expect(b1.value + b2.value).to be == "foo \nbar\n"
+      [r1, r2, w1, w2].each(&:close)
+    end
+  end
 end
