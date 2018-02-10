77b550b6754f1952cd43a842270e8304e5679330
jjy <jjyruby@gmail.com>
2018-02-10 19:32:05 +0800

    Update examples/http_requests.rb

--- examples/http_requests.rb
+++ examples/http_requests.rb
@@ -5,13 +5,13 @@ LightIO::Monkey.patch_all!
 require 'net/http'
 
 host = 'github.com'
-port = 80
+port = 443
 
 start = Time.now
 
 10.times.map do
   Thread.new do
-    Net::HTTP.start(host, port, use_ssl: false) do |http|
+    Net::HTTP.start(host, port, use_ssl: true) do |http|
       res = http.request_get('/ping')
       p res.code
     end
